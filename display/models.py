3f83910a5f59fbd32aa49cab4766eb48580ad850
demonkit <yydemonkit@gmail.com>
2016-02-19 00:09:42 +0800

    config db model definition

--- display/models.py
+++ display/models.py
@@ -8,22 +8,27 @@ from display.databases import db
 class Company(db.Model):
     __tablename__ = 'company'
 
+    id = db.Column(db.Integer, primary_key=True)
     cid = db.Column(db.Integer, unique=True)
-    logo = db.Column(db.String())
-    name = db.Column(db.String())
-    short_name = db.Column(db.String())
-    url= db.Column(db.String())
-    industry_field = db.Column(db.String())
-    size = db.Column(db.String())
-    city= db.Column(db.String())
-    finance_stage = db.Column(db.String())
+    logo = db.Column(db.String(127))
+    name = db.Column(db.String(127))
+    short_name = db.Column(db.String(127))
+    url= db.Column(db.String(127))
+    industry_field = db.Column(db.String(127))
+    size = db.Column(db.String(127))
+    city= db.Column(db.String(127))
+    finance_stage = db.Column(db.String(127))
 
 
 class Location(db.Model):
     __tablename__ = 'location'
 
-    company_id = db.Column(db.Integer, foreign_key="Company.cid")
-    brief_position = db.Column(db.String())
-    detail_position = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer)#, foreign_key="Company.cid")
+    brief_position = db.Column(db.String(127))
+    detail_position = db.Column(db.String(127))
     longitude = db.Column(db.Float)
     latitude = db.Column(db.Float)
+
+
+db.create_all()
